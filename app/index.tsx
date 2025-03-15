611099cd7a47349c7d143d672631f83e291dc222
xavier <zye.github.01@gmail.com>
2025-03-15 12:30:00 +0800

    test

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello par</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold text-4xl text-blue-700 text-center">Hello par</Text>
     </View>
   );
 }
