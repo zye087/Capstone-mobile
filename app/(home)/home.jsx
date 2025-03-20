4cd6337718d88a9f234df9ecee7df9962fa9f663
xavier <zye.github.01@gmail.com>
2025-03-20 14:21:35 +0800

    rename

--- app/(home)/home.jsx
+++ app/(home)/home.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const home = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>home</Text>
     </View>
   )
 }
 
-export default index
+export default home
--- app/index.jsx
+++ app/index.jsx
@@ -8,25 +8,38 @@ export default function Index() {
   const linkStyle = [
     'text-bold',
     'text-3xl',
-    'mt-4 border',
-    'border-white',
+    'mt-4',
     'p-4',
     'text-center',
-    'rounded-2xl',
+    'rounded-full',
     'bg-slate-800',
     'color-white',
+  ].join(' ')
+
+  const inputStyle = [
+    'rounded-full',
+    'px-4',
+    'bg-white',
+    'mb-4',
   ].join(' ')
 
   return (
     <View className="flex-1 flex-col">
       <ImageBackground className="h-full w-full flex-1 px-20 justify-center" resizeMode="cover" source={bgImgUrl}>
         <Image source={logo} className="mx-auto size-40 mb-10"/>
-        <Text className="font-bold text-4xl mb-5 text-white text-center">LOGIN PAGE</Text>
+
+        <Text className={`font-bold text-4xl mb-5 text-white text-center`}>LOGIN PAGE</Text>
+
         <Text className="text-white mb-1 text-2xl">Username</Text>
-        <TextInput style={styles.input}></TextInput>
-        <Text className="text-white mb-1 text-2xl">Password</Text>
-        <TextInput style={styles.input}></TextInput>
-        <Link className={linkStyle} href='/(home)/home'>Login</Link>
+
+        <TextInput textContentType="password" className={inputStyle}></TextInput>
+
+        <Text className=" mb-1 text-2xl">Password</Text>
+        
+        <TextInput textContentType="password" secureTextEntry={true} className={inputStyle} />
+
+        <Link className={linkStyle} href='/(home)/home'>LOGIN</Link>
+
       </ImageBackground>
     </View>
   );
