42e5b2abc7dba153344951f54ee4bc5e35f3c946
xavier <zye.github.01@gmail.com>
2025-03-20 15:57:37 +0800

    new bg, update styles, and babel

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,17 @@
-import { Link } from "expo-router";
-import { Image, Text, TextInput, View, StyleSheet, ImageBackground  } from "react-native";
+import { 
+  Link
+} from "expo-router";
 
-import bgImgUrl from '@/assets/images/bg.jpg'
+import {
+  Image,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  ImageBackground
+} from "react-native";
+
+import bg from '@/assets/images/bg.png'
 import logo from '@/assets/images/school-logo.png'
 
 export default function Index() {
@@ -16,42 +26,74 @@ export default function Index() {
     'color-white',
   ].join(' ')
 
-  const inputStyle = [
-    'rounded-full',
-    'px-4',
-    'bg-white',
-    'mb-4',
-  ].join(' ')
-
   return (
-    <View className="flex-1 flex-col">
-      <ImageBackground className="h-full w-full flex-1 px-20 justify-center" resizeMode="cover" source={bgImgUrl}>
-        <Image source={logo} className="mx-auto size-40 mb-10"/>
-
-        <Text className={`font-bold text-4xl mb-5 text-white text-center`}>LOGIN PAGE</Text>
-
-        <Text className="text-white mb-1 text-2xl">Username</Text>
-
-        <TextInput textContentType="password" className={inputStyle}></TextInput>
-
-        <Text className=" mb-1 text-2xl">Password</Text>
-        
-        <TextInput textContentType="password" secureTextEntry={true} className={inputStyle} />
-
+    // Container
+    <View
+      className={`
+        flex-1
+      `}
+    >
+      {/* BG */}
+      <ImageBackground
+        className={`
+          h-full
+          w-full
+          flex-1
+          px-20
+          justify-center
+        `}
+        resizeMode="cover"
+        source={bg}
+      >
+        {/* Logo */}
+        <Image
+          source={logo}
+          className={`
+            mx-auto
+            size-40
+            mb-10
+          `}
+        />
+        {/* Username Text */}
+        <Text
+          className={`
+            text-white
+            mb-1
+            text-2xl
+          `}
+        >
+          Username
+        </Text>
+        {/* Username Input */}
+        <TextInput
+          className={`
+            rounded-full
+            border
+            px-4
+          `}
+        />
+        {/* Password */}
+        <Text
+          className={`
+            text-white
+            mb-1
+            text-2xl
+          `}
+        >
+          Password
+        </Text>
+        {/* Password Input */}
+        <TextInput
+          className={`
+            rounded-full
+            border
+            px-4
+          `}
+          secureTextEntry
+        />
+        {/* Button */}
         <Link className={linkStyle} href='/(home)/home'>LOGIN</Link>
-
       </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 10,
-    marginBottom: 20
-  },
-});
