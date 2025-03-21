1d3769855bb95b1f5f108846edaca73896130c3b
xavier <zye.github.01@gmail.com>
2025-03-21 10:56:39 +0800

    added (profile, qr, buttons)

--- app/(home)/home.jsx
+++ app/(home)/home.jsx
@@ -1,11 +1,104 @@
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ImageBackground,
+  Image,
+  Button
+} from 'react-native'
+
 import React from 'react'
+
+import bg from '@/assets/images/bg.jpg'
+import profile from '@/assets/images/profile.png'
+import qrcode from '@/assets/images/qrCode.png'
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    // For iOS Notches
+    <SafeAreaView
+      className={`
+        flex-1
+      `}
+    >
+      {/* Container */}
+      <View
+        className={`
+          flex-1
+        `}
+      >
+        {/* BG */}
+        <ImageBackground
+          className={`
+            h-full
+            w-full
+            flex-1
+            items-center
+          `}
+          resizeMode="cover"
+          source={bg}
+        >
+          {/* Profile */}
+          <View>
+            <Image
+              source={profile}
+              resizeMode='contain'
+              className={`
+                size-28
+                border-black
+                bg-white
+                rounded-full
+              `}
+            />
+            <Text
+             className={`
+                font-bold
+                text-white
+                text-2xl
+                text-center
+                mt-3
+              `}
+            >
+              Jon Snow
+            </Text>
+          </View>
+          {/* QR Code */}
+          <View
+            className={`
+              w-64
+              bg-white
+              rounded-3xl
+              p-4
+              items-center
+            `}
+          >
+            <Image
+              source={qrcode}
+              resizeMode='contain'
+              className={`
+                size-52
+              `}
+            />
+            <Text
+             className={`
+                font-bold
+                text-4xl
+                text-center
+                mt-3
+              `}
+            >
+              My QR Code
+            </Text>
+          </View>
+          {/* Buttons */}
+          <View>
+            <Button title='Notificattions' />
+            <Button title='Manage Guardians' />
+            <Button title='Emergency Pick-up' />
+          </View>
+        </ImageBackground>
+      </View>
+    </SafeAreaView>
   )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -8,92 +8,106 @@ import {
   TextInput,
   View,
   StyleSheet,
-  ImageBackground
+  ImageBackground,
+  SafeAreaView
 } from "react-native";
 
-import bg from '@/assets/images/bg.png'
+import bg from '@/assets/images/bg.jpg'
 import logo from '@/assets/images/school-logo.png'
 
 export default function Index() {
   const linkStyle = [
-    'text-bold',
-    'text-3xl',
-    'mt-4',
-    'p-4',
-    'text-center',
-    'rounded-full',
-    'bg-slate-800',
-    'color-white',
+    
   ].join(' ')
 
   return (
     // Container
-    <View
+    <SafeAreaView
       className={`
         flex-1
       `}
     >
-      {/* BG */}
-      <ImageBackground
+      <View
         className={`
-          h-full
-          w-full
           flex-1
-          px-20
-          justify-center
         `}
-        resizeMode="cover"
-        source={bg}
       >
-        {/* Logo */}
-        <Image
-          source={logo}
+        {/* BG */}
+        <ImageBackground
           className={`
-            mx-auto
-            size-40
-            mb-10
+            h-full
+            w-full
+            flex-1
+            px-20
+            justify-center
           `}
-        />
-        {/* Username Text */}
-        <Text
-          className={`
-            text-white
-            mb-1
-            text-2xl
-          `}
+          resizeMode="cover"
+          source={bg}
         >
-          Username
-        </Text>
-        {/* Username Input */}
-        <TextInput
-          className={`
-            rounded-full
-            border
-            px-4
-          `}
-        />
-        {/* Password */}
-        <Text
-          className={`
-            text-white
-            mb-1
-            text-2xl
-          `}
-        >
-          Password
-        </Text>
-        {/* Password Input */}
-        <TextInput
-          className={`
-            rounded-full
-            border
-            px-4
-          `}
-          secureTextEntry
-        />
-        {/* Button */}
-        <Link className={linkStyle} href='/(home)/home'>LOGIN</Link>
-      </ImageBackground>
-    </View>
+          {/* Logo */}
+          <Image
+            source={logo}
+            className={`
+              mx-auto
+              size-40
+              mb-5
+            `}
+          />
+          {/* Username Text */}
+          <Text
+            className={`
+              mb-1
+              text-2xl
+            `}
+          >
+            Username
+          </Text>
+          {/* Username Input */}
+          <TextInput
+            className={`
+              rounded-full
+              border
+              px-4
+              mb-3
+            `}
+          />
+          {/* Password */}
+          <Text
+            className={`
+              mb-1
+              text-2xl
+            `}
+          >
+            Password
+          </Text>
+          {/* Password Input */}
+          <TextInput
+            className={`
+              rounded-full
+              border
+              px-4
+              mb-3
+            `}
+            secureTextEntry
+          />
+          {/* Button */}
+          <Link
+            href='/(home)/home'
+            className={`
+              text-bold
+              text-3xl
+              mt-4
+              p-4
+              text-center
+              rounded-full
+              bg-slate-800
+              color-white
+            `}
+          >
+            LOGIN
+          </Link>
+        </ImageBackground>
+      </View>
+    </SafeAreaView>
   );
 }
